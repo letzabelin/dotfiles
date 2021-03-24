52833aff3ae62bcd8e091d419dfe339aabf94b56
Andrew Zabelin <letzabelin@gmail.com>
2021-03-24 19:17:49 +0300

    change filetypes in tsserver

--- home/.config/nvim/lua/lsp/servers/typescript-ls.lua
+++ home/.config/nvim/lua/lsp/servers/typescript-ls.lua
@@ -2,7 +2,7 @@ return function(config, on_attach)
   local util = require("lspconfig/util")
   config.tsserver.setup {
     cmd = {"typescript-language-server", "--stdio"},
-    filetypes = {"typescript", "javascript"},
+    filetypes = {"typescript", "javascript", "javascriptreact", "typescriptreact"},
     root_dir = util.root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git", vim.fn.getcwd()),
     on_attach = function(client, bufnr)
       on_attach(client, bufnr)
