1d008def64c06351acf8d9adc91ec636a39e7b08
Andrew Zabelin <letzabelin@gmail.com>
2021-03-24 20:43:32 +0300

    fix signature render

--- home/.config/nvim/lua/lsp/on_attach.lua
+++ home/.config/nvim/lua/lsp/on_attach.lua
@@ -19,16 +19,17 @@ return function(client, bufnr)
   map("n", "'i", "<cmd>lua vim.lsp.buf.implementation()<CR>", options)
   map("n", "'a", "<cmd>lua require'lspsaga.codeaction'.code_action()<CR>", options)
   map("v", "'a", "<cmd>lua require'lspsaga.codeaction'.range_code_action()<CR>", options)
-  map("i", "<C-q>", "<cmd>lua require('lspsaga.signaturehelp').signature_help()<CR>", options)
 
-  -- require "timer".add(
-  -- function()
-  --   if vim.fn.mode() == "i" then
-  --     require("lspsaga.signaturehelp").signature_help()
-  --   end
-  --   return 500
-  -- end
-  -- )
+  require "timer".add(
+  function()
+    if vim.fn.mode() == "i" then
+      if not require("lspsaga.signaturehelp").has_saga_signature() then
+        require("lspsaga.signaturehelp").signature_help()
+      end
+    end
+    return 2000
+  end
+  )
 
   require "timer".add(
   function()
