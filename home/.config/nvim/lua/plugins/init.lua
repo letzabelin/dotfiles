736f1e619537cbd2c45eaad6cb0d8b84881bd051
Andrew Zabelin <a.zabelin@i-t-p.pro>
2021-06-29 17:26:29 +0300

    remove highlight for scope

--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -190,30 +190,12 @@ local ui = {
       vim.g.indentLine_enabled = 1
       vim.g.indent_blankline_char = "▏"
       vim.g.indent_blankline_use_treesitter = true
+      vim.g.indent_blankline_show_first_indent_level = false
       vim.g.indent_blankline_show_trailing_blankline_indent = false
       vim.wo.colorcolumn = "99999"
 
       vim.g.indent_blankline_filetype_exclude = {"help", "terminal", "startify"}
       vim.g.indent_blankline_buftype_exclude = {"terminal"}
-      vim.g.indent_blankline_show_current_context = true
-      vim.g.indent_blankline_context_patterns = {
-        "class",
-        "function",
-        "method",
-        "^if",
-        "while",
-        "for",
-        "with",
-        "func_literal",
-        "block",
-        "try",
-        "except",
-        "argument_list",
-        "object",
-        "dictionary"
-      }
-
-      vim.g.indent_blankline_show_first_indent_level = false
     end
   },
   -- delete buffer
